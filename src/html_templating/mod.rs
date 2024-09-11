a94bc3898d7738c1fa94c41b1b9e1c3430a1d90f
fahnestockj <jrfz31@gmail.com>
2024-09-11 10:23:04 -0400

    chore: moving off lines iter for html templating

--- src/html_templating/mod.rs
+++ src/html_templating/mod.rs
@@ -22,74 +22,67 @@ pub enum CtxValue {
 }
 
 pub fn mustachify(template_string: String, context: HashMap<&str, CtxValue>) -> String {
-    let mut new_lines: Vec<String> = vec![];
-    for line in template_string.lines() {
-        let mut new_line = String::new();
-        let mut char_iter = line.chars();
+    let mut result = String::new();
+    let mut char_iter = template_string.chars();
 
-        while let Some(char) = char_iter.next() {
-            let mut sub_iter = char_iter.clone();
-            if char == '{' && sub_iter.next().is_some_and(|next_char| next_char == '{') {
-                // check for three in a rowwww and ampersand
-                let is_triple_stache = sub_iter.clone().next().is_some_and(|char| char == '{');
-                let is_html_escaped =
-                    !(is_triple_stache || sub_iter.clone().next().is_some_and(|char| char == '&'));
+    while let Some(char) = char_iter.next() {
+        let mut sub_iter = char_iter.clone();
+        if char == '{' && sub_iter.next().is_some_and(|next_char| next_char == '{') {
+            // check for three in a rowwww and ampersand
+            let is_triple_stache = sub_iter.clone().next().is_some_and(|char| char == '{');
+            let is_html_escaped =
+                !(is_triple_stache || sub_iter.clone().next().is_some_and(|char| char == '&'));
 
-                // closing tags?
-                let closing_pattern = if is_triple_stache { "}}}" } else { "}}" };
-                // rest_of_line after the first {
-                let rest_of_line: String = char_iter.clone().collect();
-                if let Some(closing_stache_byte_idx) = rest_of_line.find(closing_pattern) {
-                    // if we have found this then char_iter is on the first { sub_iter is on the second {
-                    let skip = if is_triple_stache { 3 } else { 2 };
-                    // rest_of_line includes a starting { or {{ so - (skip-1)
-                    let char_len_until_end_of_closing_brackets =
-                        rest_of_line[..closing_stache_byte_idx].chars().count() + skip;
+            // closing tags?
+            let closing_pattern = if is_triple_stache { "}}}" } else { "}}" };
+            // rest_of_line after the first {
+            let rest_of_line: String = char_iter.clone().collect();
+            if let Some(closing_stache_byte_idx) = rest_of_line.find(closing_pattern) {
+                // if we have found this then char_iter is on the first { sub_iter is on the second {
+                let skip = if is_triple_stache { 3 } else { 2 };
+                // rest_of_line includes a starting { or {{ so - (skip-1)
+                let char_len_until_end_of_closing_brackets =
+                    rest_of_line[..closing_stache_byte_idx].chars().count() + skip;
 
-                    let content_in_stache: String = rest_of_line[..closing_stache_byte_idx]
-                        .chars()
-                        .skip(skip - 1)
-                        .collect();
+                let content_in_stache: String = rest_of_line[..closing_stache_byte_idx]
+                    .chars()
+                    .skip(skip - 1)
+                    .collect();
 
-                    // here's where we start thinking about all the different stache statements
-                    if content_in_stache.starts_with("#") {
-                        // Section!
-                    } else if content_in_stache.starts_with("^") {
-                        // Inverted Section!
-                    } else if content_in_stache.starts_with(">") {
-                        // Partial!
-                    } else if content_in_stache.starts_with("!") {
-                        // Comment!
-                    } else if content_in_stache.starts_with("$") {
-                        // Block!
-                    } else {
-                        // regular variable
-                        let key = content_in_stache.trim();
-                        let variable_value = match context.get(key) {
-                            Some(val) => val.clone(),
-                            None => CtxValue::String("".to_string()),
-                        };
-                        // makes sense for num and string but boolean?? this is smelly
-                        let string_value = match variable_value {
-                            CtxValue::Boolean(b) => b.to_string(),
-                            CtxValue::Number(n) => n.to_string(),
-                            CtxValue::String(s) => s,
-                        };
-                        new_line.push_str(&string_value);
-                        char_iter.nth(char_len_until_end_of_closing_brackets - 1);
-                        let rest: String = char_iter.clone().collect();
-                    }
+                // here's where we start thinking about all the different stache statements
+                if content_in_stache.starts_with("#") {
+                    // Section!
+                } else if content_in_stache.starts_with("^") {
+                    // Inverted Section!
+                } else if content_in_stache.starts_with(">") {
+                    // Partial!
+                } else if content_in_stache.starts_with("!") {
+                    // Comment!
+                } else if content_in_stache.starts_with("$") {
+                    // Block!
                 } else {
-                    new_line.push(char);
+                    // regular variable
+                    let key = content_in_stache.trim();
+                    let variable_value = match context.get(key) {
+                        Some(val) => val.clone(),
+                        None => CtxValue::String("".to_string()),
+                    };
+                    // makes sense for num and string but boolean?? this is smelly
+                    let string_value = match variable_value {
+                        CtxValue::Boolean(b) => b.to_string(),
+                        CtxValue::Number(n) => n.to_string(),
+                        CtxValue::String(s) => s,
+                    };
+                    result.push_str(&string_value);
+                    char_iter.nth(char_len_until_end_of_closing_brackets - 1);
                 }
             } else {
-                new_line.push(char);
+                result.push(char);
             }
+        } else {
+            result.push(char);
         }
-        new_lines.push(new_line);
     }
-    let mut result = String::new();
-    new_lines.iter().for_each(|line| result.push_str(line));
     return result;
 }
 
@@ -102,5 +95,13 @@ mod tests {
         let hash: HashMap<&str, CtxValue> = HashMap::new();
         let stache = mustachify("some text {{ stache }} some text".to_string(), hash);
         assert_eq!(stache, "some text  some text".to_string())
+    }
+
+    #[test]
+    pub fn with_var() {
+        let mut hash: HashMap<&str, CtxValue> = HashMap::new();
+        hash.insert("stache", CtxValue::String("world".to_string()));
+        let res = mustachify("hello {{ stache }} \n".to_string(), hash);
+        assert_eq!(res, "hello world \n".to_string())
     }
 }
